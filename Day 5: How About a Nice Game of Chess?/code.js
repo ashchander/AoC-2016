f5b272591603601e7e0fcc6b2d5fcd3dd18cc775
Ashwin Chander <ashchander@gmail.com>
2016-12-05 20:28:49 -0500

    Implement day 5, part 2

--- Day 5: How About a Nice Game of Chess?/code.js
+++ Day 5: How About a Nice Game of Chess?/code.js
@@ -1,12 +1,17 @@
 var crypto = require('crypto');
 
-var doorId = "wtnhxymk";
-var code = "";
+var doorId = 'wtnhxymk';
+var code = ['X','X','X','X','X','X','X','X'];
+var counter = 0;
 
-for(var i=0; code.length < 8; i++) {
+for(var i=0; counter < 8; i++) {
     var hash = crypto.createHash('md5').update(doorId + i).digest('hex');
     if(hash.startsWith('00000')) {
-        code = code + hash[5];
+        var index = parseInt(hash[5]);
+        if(code[index] === 'X') {
+            code[index] = hash[6];
+            counter++;
+        }
     }
 }
-console.log(code);
+console.log(code.join(''));
